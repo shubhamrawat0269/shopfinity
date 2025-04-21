b21af46069e691ec6e8715968257707c77f97cc6
Shubham Rawat <rawatshubam269@gmail.com>
2025-04-21 08:27:10 +0530

    Added : Shadcn added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <div className="underline">hii</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
