149e4ae965e619217be2b1645318028b4f4f1bdd
Shubham Rawat <rawatshubam269@gmail.com>
2025-08-03 16:21:09 +0530

    updated : review changes done

--- client/src/redux/slices/counterSlice.ts
+++ client/src/redux/slices/counterSlice.ts
@@ -2,6 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   value: 0,
+  isAuthenticated: false,
+  isLoading: false,
+  user: null,
 };
 
 // interface CounterState {
